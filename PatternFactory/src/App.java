1152e442bd24017c65ed4c20b0365a4f78eaacaf
AlexCh1975 <stovertikal@mail.ru>
2023-10-06 19:02:12 +0700

    add dz_2

--- PatternFactory/src/App.java
+++ PatternFactory/src/App.java
@@ -5,7 +5,12 @@ import java.util.Random;
 import Factory.GemGenerator;
 import Factory.GoldGenerator;
 import Factory.ItemGenerator;
+import Factory.LifeGenerator;
+import Factory.PowerGenerator;
+import Factory.ScullGenerator;
+import Factory.ShildGenerator;
 import Factory.SilverGenerator;
+import Factory.StarGenerator;
 
 public class App {
 
@@ -15,7 +20,6 @@ public class App {
 
         List<ItemGenerator> listItemGenerator = new ArrayList<>();
 
-        // listItemGenerator.add(new GoldGenerator());
         for (int i = 0; i < 10; i++) {
             listItemGenerator.add(new GoldGenerator());
         }
@@ -24,6 +28,21 @@ public class App {
         }
         for (int i = 0; i < 5; i++) {
             listItemGenerator.add(new GemGenerator());
+        }
+        for (int i = 0; i < 1; i++) {
+            listItemGenerator.add(new StarGenerator());
+        }
+        for (int i = 0; i < 1; i++) {
+            listItemGenerator.add(new ScullGenerator());
+        }
+        for (int i = 0; i < 7; i++) {
+            listItemGenerator.add(new LifeGenerator());
+        }
+        for (int i = 0; i < 7; i++) {
+            listItemGenerator.add(new PowerGenerator());
+        }
+        for (int i = 0; i < 7; i++) {
+            listItemGenerator.add(new ShildGenerator());
         }
 
         Random ran = new Random();
